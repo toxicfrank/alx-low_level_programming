70797ae980d1232acced8db299779b26f449e4f5
toxicfrank <ngugifrancis930@gmail.com>
2023-03-18 01:31:54 -0700

    task alx

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that print a line using the printf function
+ *
+ * Returns: Always 0 (successs)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
