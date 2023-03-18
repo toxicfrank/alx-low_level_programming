09d98293ae99599aa30f09f377dfaedff38848f0
toxicfrank <ngugifrancis930@gmail.com>
2023-03-18 00:48:53 -0700

    task alx

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return:always 0 (Success)
+ */
+int main(void)
+{
+	char str[] = "Programming is like building a multilingual puzzle\n";
+
+	puts(str);
+	return (0);
+
+}
