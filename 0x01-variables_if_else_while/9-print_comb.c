c69f624412e338aa8dd1634c88f8a36f336636b8
toxicfrank <ngugifrancis930@gmail.com>
2023-03-19 10:42:04 -0700

    task alx

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,14 +10,13 @@ int main(void)
 
 	for (ch = 0; ch < 10; ch++)
 	{
-		if (ch == 9)
-			putchar(ch + '0');
-		else
+		putchar(ch + '0');
+		if (ch != 9)
 		{
-			putchar(ch + '0');
 			putchar(',');
 			putchar(' ');
 		}
+		putchar('\n');
 	}
 	return (0);
 }
