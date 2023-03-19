84028b85774f5bdf4e30e035fab229e7079be53e
toxicfrank <ngugifrancis930@gmail.com>
2023-03-19 02:22:58 -0700

    task alx

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,12 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
-int main(void) /* main - Entry point */
+/**
+ * main - main function
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
 {
 		int n;
 
