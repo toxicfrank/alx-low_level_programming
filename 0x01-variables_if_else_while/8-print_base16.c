b849fcc499fbf25e0d36bea887e6463d45c478a1
toxicfrank <ngugifrancis930@gmail.com>
2023-03-19 10:01:12 -0700

    task alx

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int ch;
+	int sh;
+
+	for (ch = 0; ch <= 10; ch++)
+		putchar(ch + '0');
+	for (sh = 'a'; sh <= 'f'; sh++)
+		putchar(sh);
+	putchar('\n');
+
+	return (0);
+}
