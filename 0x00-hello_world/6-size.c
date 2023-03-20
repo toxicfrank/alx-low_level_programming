590900885a4e340a3eb25893271a460a3bb992cd
toxicfrank <ngugifrancis930@gmail.com>
2023-03-19 20:49:35 -0700

    task alx

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ long long int d;
 float f;
 
 printf("Size of a char: %zu byte(s)\n", (unsigned long) sizeof(a));
-printf("Size of a int: %zu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of an int: %zu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long int: %zu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of a long long int: %zu byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of a float: %zu byte(s)\n", (unsigned long)sizeof(f));
