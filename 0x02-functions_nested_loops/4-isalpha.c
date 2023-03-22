58b1ac7f66070374bfb264420c2b866a466aaa45
toxicfrank <ngugifrancis930@gmail.com>
2023-03-22 11:54:03 -0700

    task alx

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,5 +1,15 @@
 #include "main.h"
+/**
+ * _isalpha - prints out alphabet
+ * @c: prints character
+ *
+ * Return: always 0
+ * otherwise return 1
+ */
 int _isalpha(int c)
 {
-	if (c < )
+	if (c >= 97 && c <= 122 && c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
 }
