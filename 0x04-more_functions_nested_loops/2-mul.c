e7a0f46b36c30186465914063b41790d0a294477
toxicfrank <ngugifrancis930@gmail.com>
2023-03-23 09:26:09 -0700

    task alx

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - prints multiplication of a and b
+ *
+ * @a: function parameter
+ * @b: function parameter
+ *
+ * Return: a*b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
