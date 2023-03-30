1b2bf6e170167799dd137af992005b69e023caa8
toxicfrank <ngugifrancis930@gmail.com>
2023-03-30 11:36:29 -0700

    task alx

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * string_toupper - change lower case to upper case
+ * @str: string to be converted
+ * Return: strings
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+	}
+	return (str);
+}
