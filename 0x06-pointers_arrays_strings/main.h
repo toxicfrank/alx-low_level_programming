10d5ebc66631371bac25aa41fda7d7c9206ee859
toxicfrank <ngugifrancis930@gmail.com>
2023-03-30 11:49:34 -0700

    task alx

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,5 +8,7 @@ char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 char *string_toupper(char *);
 void reverse_array(int *a, int n);
+char *cap_string(char *);
+char *leet(char *);
 
 #endif
