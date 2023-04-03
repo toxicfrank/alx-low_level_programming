5247172e3ef1284cdf8abaabd1501a51932e653c
toxicfrank <ngugifrancis930@gmail.com>
2023-04-03 11:45:24 -0700

    task alx

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * _memset - Entry point
+ * @s: pointed destination
+ * @b: constant type
+ * @n: bytes
+ * Return: always 0
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
