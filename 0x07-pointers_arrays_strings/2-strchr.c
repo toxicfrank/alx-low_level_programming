7b5753f017bd2db4ca15175fe76ff55e1e7862e1
toxicfrank <ngugifrancis930@gmail.com>
2023-04-03 12:01:29 -0700

    task alx

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: always 0
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + 1);
+	}
+	return  (NULL);
+}
