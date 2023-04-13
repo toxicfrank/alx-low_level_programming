1ed33334cd8ca8a69bea7a537dc4cf312afc58c2
toxicfrank <ngugifrancis930@gmail.com>
2023-04-13 13:58:00 +0100

    alx

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *malloc_checked - shoul cause normal process termination with
+ * a status value of 98
+ * @b: parameter function
+ * Return: pointer to allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p = malloc(b);
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return (p);
+}
