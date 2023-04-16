88b7592a1c2397806612424332666f11dc362b93
toxicfrank <ngugifrancis930@gmail.com>
2023-04-16 14:28:39 +0100

    alx

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -12,23 +12,23 @@ int main(int argc, char *argv[])
 	unsigned long	multiply;
 	int num1, num2;
 
-	if (argc != 3)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	for (num1 = 1; num1 < argc; num1++)
-	{
-		for (num2 = 0; argv[num1][num2] != '\0'; num2++)
+		if (argc != 3)
 		{
-			if (argv[num1][num2] > 5? || argv[num1][num2] < 48)
+			printf("Error\n");
+			exit(98);
+		}
+		for (num1 = 1; num1 < argc; num1++)
+		{
+			for (num2 = 0; argv[num1][num2] != '\0'; num2++)
 			{
-				printf("Error\n");
-				exit(98);
+				if (argv[num1][num2] > 5 || argv[num1][num2] < 48)
+				{
+					printf("Error\n");
+					exit(98);
+				}
 			}
 		}
-	}
-	multiply = atoi(argv[1]) * atoi(argv[2]);
-	printf("%lu\n", multiply);
-	return (0);
+		multiply = atol(argv[1]) * atol(argv[2]);
+		printf("%lu\n", multiply);
+		return (0);
 }
