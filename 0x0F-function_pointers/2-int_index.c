49ef89861d613f010a81c10f58bffa04083d4ce4
toxicfrank <ngugifrancis930@gmail.com>
2023-04-21 09:06:51 +0100

    alx

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * int_index - main function
+ * @array: function parameter
+ * @size: function parameter
+ * @cmp: function parameter
+ * Return: int
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}
