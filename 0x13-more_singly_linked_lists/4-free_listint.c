b8b782f47e0909f5d9a2b24a1c3e975e528f64e3
toxicfrank <ngugifrancis930@gmail.com>
2023-05-01 10:15:36 +0100

    alx

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -10,25 +10,12 @@
 
 void free_listint(listint_t *head)
 {
-/*initialize the current pointer with head*/
-	listint_t *current = head;
+	listint_t *temp;
 
-/*initialize the next pointer*/
-	listint_t *next;
-
-/*While current is not NULL*/
-	while (current == NULL)
+	while (head)
 	{
-	/*assign the next address to next pointer*/
-		next = current->next;
-
-	/*free the current pointer*/
-		free(current);
-
-	/*assign the next pointer to current pointer*/
-		current = next;
+		temp = head->next;
+		free(head);
+		head = temp;
 	}
-
-/*assign NULL to head*/
-	head = NULL;
 }
