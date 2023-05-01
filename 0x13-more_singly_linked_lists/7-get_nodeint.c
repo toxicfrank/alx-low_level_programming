776262dd95f231247cc774686a6661de54b1699c
toxicfrank <ngugifrancis930@gmail.com>
2023-05-01 10:56:07 +0100

    alx

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,22 +1,22 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index - returns the nth node of a listint_t linked list.
- * @head: pointes to the node
- * @index: function parameter that contains integer
- * Return: node
+ * get_nodeint_at_index - returns the nth node of a listint_t linked list
+ * @head: pointer to head of list
+ * @index: index of the node, starting at 0
+ * Return: pointer to the nth node of a listint_t linked list
  */
-
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int a;
+	unsigned int i;
 
-	listint_t *curr = head;
-
-	while (curr && a < index)
+	if (head == NULL)
+		return (NULL);
+	for (i = 0; i < index; i++)
 	{
-		curr = curr->next;
-		a++;
+		if (head->next == NULL)
+			return (NULL);
+		head = head->next;
 	}
-	return (curr ? curr : NULL);
+	return (head);
 }
