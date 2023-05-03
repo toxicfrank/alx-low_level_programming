78b08fce829dbad192f4e7f815d17982defc51dd
toxicfrank <ngugifrancis930@gmail.com>
2023-05-03 09:58:44 +0100

    practice

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,24 @@
 #include "main.h"
 /**
+ * jack_bauer - function that prints every minute of the day of Jack Bauer,
+ * starting from 00:00 to 23:59.
+ * @void: function parameter
+ * Return: void
  */
-void times_table(void)
+void jack_bauer(void)
 {
+	int a, b;
 
+	for (a = 0; a < 24; a++)
+	{
+		for (b = 0; b < 60; b++)
+		{
+			_putchar((a / 10) + '0');
+			_putchar((a % 10) + '0');
+			_putchar(':');
+			_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
+			_putchar('\n');
+		}
+	}
 }
