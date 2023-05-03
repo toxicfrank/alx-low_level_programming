d9f4a4bec98814fb944b14b6af55242c359389a2
toxicfrank <ngugifrancis930@gmail.com>
2023-05-03 12:43:42 +0100

    practice

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - function that adds two integers and returns the result
+ * @a:first integer
+ * @b: second integer
+ * Return: always 0
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
