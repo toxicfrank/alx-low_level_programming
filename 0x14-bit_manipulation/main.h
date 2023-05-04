5d0f366b57b34cc268199a1a0ff14bf17beda0ed
toxicfrank <ngugifrancis930@gmail.com>
2023-05-04 19:47:43 +0100

    practice

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+#include <stdarg.h>
+
+unsigned int binary_to_uint(const char *b);
+
+#endif
